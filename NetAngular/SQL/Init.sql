0ba1a8643838d0efc8bf4471ac4a11891a305d42
Janez Vegan <janez.vegan@gmail.com>
2022-12-01 22:40:11 +0100

    init sql db 2

--- NetAngular/SQL/Init.sql
+++ NetAngular/SQL/Init.sql
@@ -9,14 +9,32 @@ BEGIN
 	use [ParentChildren]
 END
 
+--****************************************************************************************
+--	Create tables
+
+
 IF OBJECT_ID('Parent', 'U') IS NOT NULL 
-	DROP TABLE Parent
+begin
+	DROP TABLE Children;
+	DROP TABLE Parent;
+end
 IF OBJECT_ID('Parent', 'U') IS NULL 
 BEGIN
  create table Parent(
  id int NOT NULL PRIMARY KEY,
  firstName varchar(40),
  lastName varchar(40),
+)
+END
+
+IF OBJECT_ID('Children', 'U') IS NULL 
+BEGIN
+	 create table Children(
+	 id int NOT NULL PRIMARY KEY,
+	 firstName varchar(40),
+	 lastName varchar(40),
+	 dateofBirth date,
+	 ParentId int FOREIGN KEY REFERENCES Parent(id)
 )
 END
 
