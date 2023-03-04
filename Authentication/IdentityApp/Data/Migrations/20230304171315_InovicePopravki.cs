bdccac83d4c6091cd83b65a85fa5e3e0ba25c5cc
Janez Vegan <janez.vegan@gmail.com>
2023-03-04 20:01:24 +0100

    Add IndetityApp application

---
+++ Authentication/IdentityApp/Data/Migrations/20230304171315_InovicePopravki.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityApp.Data.Migrations
+{
+    public partial class InovicePopravki : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MyProperty",
+                table: "Invoice",
+                newName: "InoviceAmount");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InoviceMonth",
+                table: "Invoice",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "InvoiceOwner",
+                table: "Invoice",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InvoiceOwner",
+                table: "Invoice");
+
+            migrationBuilder.RenameColumn(
+                name: "InoviceAmount",
+                table: "Invoice",
+                newName: "MyProperty");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InoviceMonth",
+                table: "Invoice",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
---
+++ Authentication/IdentityApp/Invoice.cs
@@ -0,0 +1,11 @@
+﻿namespace IdentityApp
+{
+    public class Invoice
+    {
+        public int InvoiceId { get; set; }
+        public double InoviceAmount { get; set; }
+
+        public string? InoviceMonth { get; set; }
+        public string? InvoiceOwner { get; set; }
+    }
+}
